5df7c3c739ca6e4d1c932e38a69e356a27106246
afnanaish <afnanjaber6@gmail.com>
2023-12-24 03:12:49 +0300

    Add ai-player & add documentation

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Tic-Tac-Toe
 Tic-Tac-Toe is a classic paper-and-pencil game played by two players, traditionally 'X' and 'O', on a 3x3 grid. The objective is to create a sequence of three of your symbols in a row, column, or diagonal, before your opponent does.
+
+
+steps :
+ When you run the game  the  'implementBord()' function it will be called then the 3*3bord will display with 9 positions and the numbers of each position .
+
+                                                                     { stp-1 using 'playWithAi()' function}
+1. First, the question is asked, “2 players or 1 player?”           
+
+    case-One: If he chooses 2
+                                                                 { steps-2&3  using 'chosePlayer();'function }
+2. The question is: “Play with x or o.”                            
+3. If he enters other than these two letters, it will print “Enter only x or o!”, The step 2 & 3 is repeated until x or o is entered   (by calling the same function 'chosePlayer();' another time)
+
+                                                                 {  steps 4-10  inside  play_turn (); function }
+4. If user enter x , “Enter an available position number for X-player" and  “Enter an available position number for O-player" if he chose O .
+                                                               {steps 5-7 checkValidateInput() function }
+5. After a number is entered, it is checked whether the number is available from the positions (initially all numbers 1-9 are available).          
+6. If the number is not available, “Enter the number of one of the available positions” message will be printed, then steps 5 and 6 will be repeated until he entered an available .
+7. If a number is entered from the available positions, the player code will be display on this position, the position number will be deleted from the list of available position numbers (List <int> availableLoc), and the (fullLocations variable) will be incremented by one.
+
+                                                                              {check_patterns() function }
+8. It is checked if one of the winning patterns is achieved for one of the players.
+     A - If verified, the name of the winning player will be printed and the user will be asked if he wants to play again. Enter the letter “A”.
+          A-1- If you enter the letter A, the steps will be repeated from the beginning (Step 1)
+          A-2- If you enter another letter, the game will be stopped
+    B - If none of the winning patterns is achieved, we will move to the next step.
+
+9. - It will be checked if fullLocations = 9 (since it has reached this step, of course, none of the players will have achieved any winning pattern) and therefore “No one won” will be printed. “If you want to restart the game, enter A.” { playAgain(); function}
+
+                                                                                     {calling play_turn();}
+10. - If (fullLocations = 9) does not become, then the steps from step 2 will be repeated 
+
+
+      case-two: If he chooses to play with one player (isAI==true), user will be asked “ play with x or o?”
+1. Play will begin by the user
+   a- (if user chose X then he will play first) by following the previous steps from step 2-10, then the turn will be transferred to the computer player (ai).
+   b-  if user chose O , then the computer player (x-player ) will start first.
+2. The computer will give a random number from 4 to 10, and the function 'checkValidateInput()' will check if the given number is among the available numbers by repeating steps from 5 to 7 until the computer gives an available location number, until step-10 when the turn will be transferred to the user. And so on by repeating steps 4 to 10 until the game is stopped by one of conditions is satisfied .
--- xo.dart
+++ xo.dart
@@ -1,24 +1,29 @@
-import 'dart:io';
-// use import 'dart:io' because user input is required
+import 'dart:io'; //  because user input is required
+import 'dart:math'; // to use random
 
 void main() {
+  print ('XO Game');
   implementBord(); // implement the game empty body
-  play_turn ();
+  isAI = playWithAi();  // check if user want to play with computer or with friend
+  chosePlayer();  // user chose to be X-player or O-player
+  play_turn (); 
 }
 
- List <String> locations =['1','2','3','4','5','6','7','8','9'];
- List <int> availableLoc =[0,1,2,3,4,5,6,7,8];
+ List <String> locations =['1','2','3','4','5','6','7','8','9'];  //uses to deal with the bord-positions values
+ List <int> availableLoc =[1,2,3,4,5,6,7,8,9];  // list of available positions in bord  (when one user put its code on one of positions then this position will be deleted)
  List <List<int>> winPatterns =[ [0,1,2] , [3,4,5] , [6,7,8] , [0,4,8] , [2,4,6] , [0,3,6] , [1,4,7] , [2,5,8] ]; // list of (win's patterns lists)
 
 
  // variables
 bool isX = true; //Represents if  X-player has the turn , else its the O-player turn
-bool winner = false; 
+bool winner = false;  // no player is satisfy one of the win patterns
 int locationsFull = 0; // the number of the locations that is full (used cant selected second time)
+bool isAI = false;   // false mean 2 player , true mean 1 players
+bool player1turn = true;  // true mean x-player , false mean 0-player
+
 
 void implementBord(){
 
-  print ('XO Game');
   print(' ${locations[0]} | ${locations[1]} | ${locations[2]} ');
   print('---|---|---');
   print(' ${locations[3]} | ${locations[4]} | ${locations[5]} ');
@@ -28,9 +33,25 @@ void implementBord(){
 }
 
 // function for each turn // to set the player who is have the turn & the player enter the location he want
-void play_turn (){
-  print ('enter available position number from bord for ${isX==true ? "X" : "O"}-player');
-   int locate = int.parse(stdin.readLineSync()!); // user enter the number of its location
+void play_turn(){
+  // print ('enter available position number from bord for  "${player1}" -player');
+  int locate;
+  if (isAI==true) {
+    if (!player1turn) {
+      print ('The turn for ${player1turn==true ? "X" : "O"}-AI-player');
+      Random random= Random();
+      locate= random.nextInt(9);
+      print('${player1turn==true ? "X" : "O"}-AI-player chose $locate position');
+    } else {
+      print ('enter available position number from bord for ${player1turn==true ? "X" : "O"}-player');
+      locate = int.parse(stdin.readLineSync()!);// user enter the number of its location
+      }
+  }else{
+    print ('enter available position number from bord for ${player1turn==true ? "X" : "O"}-player');
+    locate = int.parse(stdin.readLineSync()!); // user enter the number of its location
+  }
+
+
 
    bool validity =checkValidateInput(locate);
     while(validity==false){
@@ -40,23 +61,31 @@ void play_turn (){
 
   availableLoc.remove(locate);
   locations[locate-1]='X';
-  print( locations[locate-1]);
+
   if(isX){
+  //if(player1turn){
   locations[locate-1]='X';}
   else{
   locations[locate-1]='O';}
   isX = !isX;
+  player1turn =!player1turn;
   locationsFull++;
+  print('the number of reserved positions = ${(locationsFull-9).abs()} ');
+  print('the number of available positions = ${locationsFull} ');
+
+  check_patterns();
 
   if(locationsFull == 9){
     // if locations all comes full that mean the game ended & you will play again
-    winner= true ; // to stop the play_turn() function
+    print ('---------------------------');
     print ('no player won , play again ');
+    print ('---------------------------');
+    // restart the game 
+    playAgain();
   } else {
   implementBord();
   }
 
-  check_patterns();
 
   if (winner==false){
   play_turn();
@@ -64,7 +93,10 @@ void play_turn (){
 }
 
 
-bool check_patterns(){
+
+
+
+bool check_patterns(){   // check if any of players satisfy one of wins patterns
   bool result =false;
 
   for (var pattern in winPatterns){
@@ -76,15 +108,24 @@ bool check_patterns(){
     result=true;
     winner = true;
     print('${locations[a]}-player is the winner !! ');
-    break;  // if any pattern is satisfied then there are a winner player & game must stopped
+    print ('');
+    print('the game ended !! ');
+    print ('');
+    print('if you want to play again click A . ');
+      if(stdin.readLineSync()=='A'){
+         playAgain();}
+         else{
+              break;  // if any pattern is satisfied then there are a winner player & game must stopped
+              }
     }else {
-
+      // result=false;
+      // winner = false;
     }
   }
   return result;
 }
 
-// function to check if the number enterd by user is available
+//function to check if the number entered by user is available
 bool checkValidateInput(int location){
   bool status=true;
   if (availableLoc.contains(location)==false){
@@ -94,4 +135,50 @@ bool checkValidateInput(int location){
 return status;
 }
 
-// 012  345  678    048 246   036 147 258
+void playAgain(){  // function to reset the bord as the game beginning
+  locations =['1','2','3','4','5','6','7','8','9']; // the number of each position appear on it
+  availableLoc =[1,2,3,4,5,6,7,8,9]; // set all positions as available
+  locationsFull = 0; // no position must be booked up at the first of playing 
+  winner=false;
+  print ('XO Game');
+  implementBord(); // implement the game empty body
+  play_turn (); 
+}
+
+void chosePlayer (){   //  chose play with x-player   or O-player
+print('Play with X or O ?');
+var player=stdin.readLineSync();
+if(player=='X'||player=='x'){
+isX=true;
+player1turn =true;
+
+} else if (player=='O'||player=='o'){
+isX=false;
+player1turn =false;
+
+} else {
+print('enter X or O !!');
+chosePlayer();
+}
+
+}
+
+bool playWithAi(){         // if user chose 1player >> mean (play with ai)
+  bool status = false;
+
+  print (' 1 player or 2 players ?');
+  print ('--- enter `1` or `2` ----');
+  int ai= int.parse(stdin.readLineSync()!);
+  if(ai==1){
+    status = true;
+  } else if (ai==2){
+    status = false;
+  } else {
+    print ('--- enter `1` or `2` only! ----');
+    playWithAi();
+  }
+  return status;
+}
+
+
+
